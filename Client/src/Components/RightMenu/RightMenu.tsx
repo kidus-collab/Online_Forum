a03f8d984ed3e8f702067361fb62101c8b0aae03
kidus-collab <guyfreaky894@gmail.com>
2023-09-30 18:16:57 +0300

    use Effect hook to pass the state

--- Client/src/Components/RightMenu/RightMenu.tsx
+++ Client/src/Components/RightMenu/RightMenu.tsx
@@ -7,32 +7,47 @@ import { getTopCategories } from "../../Services/DataService";
 import TopCategory from "./TopCategory";
 
 // lodash
-import { groupBy } from "lodash/groupBy";
+import groupBy from "lodash/groupBy";
 
 // react modal for our right menu
 
 const RightMenu = () => {
-  const [topCategories, setTopCategories] = useState<JSX.Element[] | null>(
-    null
-  );
+  const [topCategories, setTopCategories] = useState<
+    JSX.Element[] | undefined
+  >();
 
   useEffect(() => {
     getTopCategories()
-      .then()
+      .then((res) => {
+        // lodash function to group the data by category
+        const topCategoryThread = groupBy(res, "category");
+        const rightMenuElements = [];
+        /* to sort current top by index of the array & pas it to top Category 
+            then push to array of jsx element 
+            */
+        for (let key in topCategoryThread) {
+          const currentTopThread = topCategoryThread[key];
+          rightMenuElements.push(
+            <TopCategory key={key} topThread={currentTopThread} />
+          );
+        }
+
+        setTopCategories(rightMenuElements);
+      })
       .catch((err) => `Error Caught : ${err}`);
   }, [topCategories]);
 
   return (
     <Box
       alignSelf="end"
-      w="16%"
+      w="18%"
       h="100%"
       bgColor={color[2]}
       display={{ base: "none", md: "block" }}
       boxShadow="xl"
       rounded="lg"
     >
-      <TopCategory />
+      {topCategories}
     </Box>
   );
 };
