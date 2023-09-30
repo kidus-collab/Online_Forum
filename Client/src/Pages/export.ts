f7e50aaa7a686c8601f8f5b074c4e0c32e057102
kidus-collab <guyfreaky894@gmail.com>
2023-09-30 18:23:45 +0300

    change of route in pages

--- Client/src/Pages/export.ts
+++ Client/src/Pages/export.ts
@@ -1,6 +1,6 @@
 export { default as Login } from "./auth/Login";
-export { default as Register } from "./RegisterPage/Register";
+export { default as Register } from "./auth/Register";
 export { default as Post } from "./Post/Post";
 export { default as Profile } from "./Profile";
 export { default as Home } from "./Home";
-export { default as EmailRegister } from "./RegisterPage/EmailRegister";
+export { default as EmailRegister } from "./auth/EmailRegister";
