a41e8863ba63ae5aea0930b534ae5bdacd87b509
kidus-collab <guyfreaky894@gmail.com>
2023-09-30 18:17:27 +0300

    final touch to component

--- Client/src/Components/RightMenu/TopCategory.tsx
+++ Client/src/Components/RightMenu/TopCategory.tsx
@@ -1,14 +1,66 @@
 import React, { FC, useState, useEffect } from "react";
-import { getTopCategories } from "../../Services/DataService";
+//import { getTopCategories } from "../../Services/DataService";
+import CategoryThread from "../../Models/CategoryThread";
+import { VStack, Box, Text } from "@chakra-ui/react";
+
+import { Font, color } from "../../utils/constants";
 
 interface topCategoryProps {
-  ThreadId: string;
-  category: string;
-  ThreadTitle: string;
+  key: string;
+  topThread: Array<CategoryThread>;
 }
 
-const TopCategory: FC<topCategoryProps> = () => {
-  return <div>TopCategory</div>;
+const TopCategory: FC<topCategoryProps> = ({ topThread }) => {
+  const [threads, setThreads] = useState<JSX.Element | undefined>();
+
+  useEffect(() => {
+    if (topThread && topThread.length > 0) {
+      const threadElement = topThread.map((top) => {
+        return (
+          <Box key={top.ThreadId}>
+            <Text
+              my={3}
+              _hover={{ color: "blue", textDecoration: "underline" }}
+              mx="auto"
+              fontFamily={Font}
+              fontWeight="bold"
+              color={color[5]}
+              fontSize="14px"
+            >
+              {top.ThreadTitle}
+            </Text>
+          </Box>
+        );
+      });
+      setThreads(
+        <Box
+          alignItems="left"
+          justifyContent="left"
+          ml={4}
+          overflowX="clip"
+          mt={4}
+          mb={2}
+        >
+          {threadElement}
+        </Box>
+      );
+    }
+  }, [topThread, threads]);
+  return (
+    <VStack alignItems="left" justifyContent="left">
+      <Text
+        mt={5}
+        ml={4}
+        color="white"
+        as="strong"
+        fontFamily={Font}
+        fontSize="20px"
+      >
+        {topThread[0].category}
+      </Text>
+      {threads}
+    </VStack>
+  );
 };
 
 export default TopCategory;
