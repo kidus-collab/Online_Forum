9e2d05585aaabd9a33af847b293510cc0665460b
kidus-collab <guyfreaky894@gmail.com>
2023-10-02 20:31:50 +0300

    mod:Icon type

--- Client/src/Models/Category.ts
+++ Client/src/Models/Category.ts
@@ -1,3 +1,5 @@
+import { IconType } from "react-icons";
+
 export default class Category {
-  constructor(public id: string, public name: string, public Icon: any) {}
+  constructor(public id: string, public name: string, public Icon: IconType) {}
 }
