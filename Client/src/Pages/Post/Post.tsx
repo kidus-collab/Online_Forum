08d3d564c7ed860533cd2d238c2344eee66307bb
kidus-collab <guyfreaky894@gmail.com>
2023-10-02 18:08:10 +0300

    fix: need fixing

--- Client/src/Pages/Post/Post.tsx
+++ Client/src/Pages/Post/Post.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
+import PostHeader from "./PostHeader";
+import PostCategory from "./PostCategory";
+import { useParams } from "react-router-dom";
+import { VStack } from "@chakra-ui/react";
 const Post = () => {
-  return (
-    <div>Post</div>
-  )
-}
+  const { id } = useParams();
 
-export default Post
+  /*  useEffect(()=> {
+      if(id && id > 0) { 
+
+      }
+  } , [id])
+  */
+  return (
+    <VStack>
+      <PostHeader />
+      <PostCategory />
+    </VStack>
+  );
+};
+
+export default Post;
