fe7427395c8fc7adffa04344b4b6c8320b8585cd
kidus-collab <guyfreaky894@gmail.com>
2023-10-02 18:01:46 +0300

    Add: New component in post page

---
+++ Client/src/Pages/Post/PostCategory.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PostCategory = () => {
+  return <div>PostCategory</div>;
+};
+
+export default PostCategory;
---
+++ Client/src/Pages/Post/PostHeader.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+// import getTimePastIfLessThanDay
+
+/* 
+  component will display  
+  thread title 
+  username 
+  Last modified on
+  */
+const PostHeader = () => {
+  return <div>PostHeader</div>;
+};
+
+export default PostHeader;
