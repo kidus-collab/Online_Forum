e458ce7caf63e44014cc6bf7e929b65073c98113
kidus-collab <guyfreaky894@gmail.com>
2023-10-03 22:10:30 +0300

    fix:prop title

--- Client/src/Pages/Post/PostTitle.tsx
+++ Client/src/Pages/Post/PostTitle.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from "react";
 
+import { Font, color } from "../../utils/constants";
 import { Input, Box, Text } from "@chakra-ui/react";
 interface PostHeaderProps {
   title: string | undefined;
@@ -8,8 +9,24 @@ interface PostHeaderProps {
 const PostTitle: FC<PostHeaderProps> = ({ title }) => {
   return (
     <Box>
-      <Text>Title</Text>
-      <Input />
+      <Text
+        fontFamily={Font}
+        fontWeight="extrabold"
+        fontSize="18px"
+        color="white"
+        mb={1}
+      >
+        Title
+      </Text>
+      <Input
+        type="text"
+        bgColor={color[2]}
+        variant="filled"
+        color="white"
+        fontWeight="bold"
+        fontFamily={Font}
+        placeholder="Thread 1"
+      />
     </Box>
   );
 };
