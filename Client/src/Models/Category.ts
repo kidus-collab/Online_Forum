4986a4e1e4a07ee767300ba6292393d84ea806f3
kidus-collab <guyfreaky894@gmail.com>
2023-10-03 22:09:51 +0300

    fix:Icontype

--- Client/src/Models/Category.ts
+++ Client/src/Models/Category.ts
@@ -1,5 +1,5 @@
-import { IconType } from "react-icons";
+//import { IconType } from "react-icons";
 
 export default class Category {
-  constructor(public id: string, public name: string, public Icon: IconType) {}
+  constructor(public id: string, public name: string, public Icon: any) {}
 }
