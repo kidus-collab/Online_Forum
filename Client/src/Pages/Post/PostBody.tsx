b6afb9f0f5ecf0a77f8e917f193c4881ed77828f
kidus-collab <guyfreaky894@gmail.com>
2023-10-04 20:08:27 +0300

    mod:component style&content

--- Client/src/Pages/Post/PostBody.tsx
+++ Client/src/Pages/Post/PostBody.tsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { FC } from "react";
 
-const PostBody = () => {
-  return <div>PostBody</div>;
+import { Box, Text, Textarea } from "@chakra-ui/react";
+
+import { Font, color } from "../../utils/constants";
+interface threadBodyProps {
+  body?: string;
+}
+
+const PostBody: FC<threadBodyProps> = ({ body }) => {
+  return (
+    <Box my={2}>
+      <Text
+        fontFamily={Font}
+        fontWeight="extrabold"
+        fontSize="18px"
+        color="white"
+        my={2}
+      >
+        {body ? body : "Body"}
+      </Text>
+      <Textarea
+        bgColor={color[2]}
+        variant="filled"
+        color="white"
+        fontWeight="bold"
+        fontFamily={Font}
+      />
+    </Box>
+  );
 };
 
 export default PostBody;
