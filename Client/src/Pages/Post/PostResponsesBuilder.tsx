6a506eb82dd8a49281cbb7fd8194b728372d62af
kidus-collab <guyfreaky894@gmail.com>
2023-10-06 20:45:47 +0300

    add:component

---
+++ Client/src/Pages/Post/PostResponsesBuilder.tsx
@@ -0,0 +1,9 @@
+import React, { FC, useState, useEffect } from "react";
+
+interface PostResponsesBuilder {}
+
+const PostResponsesBuilder = () => {
+  return <div>PostResponsesBuilder</div>;
+};
+
+export default PostResponsesBuilder;
