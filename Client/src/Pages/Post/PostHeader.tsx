c0cb310ab00d3011c94becf54c26866ce4e5b202
kidus-collab <guyfreaky894@gmail.com>
2023-10-06 20:50:16 +0300

    fix:props

--- Client/src/Pages/Post/PostHeader.tsx
+++ Client/src/Pages/Post/PostHeader.tsx
@@ -1,8 +1,10 @@
 import React, { FC } from "react";
 
-import { Text, Flex, Button, Box, Avatar } from "@chakra-ui/react";
+import { Text, Box } from "@chakra-ui/react";
 
 import { Font, color } from "../../utils/constants";
+
+import UserNameandTime from "./userNameandTime";
 
 // import getTimePastIfLessThanDay
 
@@ -14,14 +16,15 @@ import { Font, color } from "../../utils/constants";
   */
 
 interface PostHeaderProps {
+  title: string | undefined;
   userName?: string;
-  lastModifiedOn: Date;
-  title?: string;
+  lastModifiedOn?: Date;
 }
+
 const PostHeader: FC<PostHeaderProps> = ({
+  title,
   userName,
   lastModifiedOn,
-  title,
 }) => {
   return (
     <Box mx="auto" mb={3} justifyContent="left" alignItems="left">
@@ -35,44 +38,7 @@ const PostHeader: FC<PostHeaderProps> = ({
       >
         {title ? title : "thread 1"}
       </Text>
-      <Box my={8} display="Flex" justifyContent="space-between">
-        <Flex mr={{ base: "6rem", md: "8rem" }}>
-          <Box pos="relative">
-            <Avatar />
-            <Button
-              pos="absolute"
-              right={0}
-              bottom={0}
-              fontSize="13px"
-              fontWeight="extrabold"
-              color="white"
-              rounded="100%"
-              w={3}
-              h="30px"
-              bgColor={color[3]}
-            >
-              +
-            </Button>
-          </Box>
-          <Text
-            fontWeight="extrabold"
-            fontSize={{ base: "13px", md: "15px" }}
-            color={color[5]}
-            fontFamily={Font}
-            ml={5}
-          >
-            {userName ? userName : "Kidus G"}
-          </Text>
-        </Flex>
-        <Box>
-          <Text
-            fontWeight="extrabold"
-            fontSize={{ base: "13px", md: "15px" }}
-            color={color[5]}
-            fontFamily={Font}
-          >{`0 min ago`}</Text>
-        </Box>
-      </Box>
+      <UserNameandTime UserName={userName} lastModified={lastModifiedOn} />
     </Box>
   );
 };
