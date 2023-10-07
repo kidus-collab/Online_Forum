d64168c7bca6883c1b4cb8ba1c252b135b37c24a
kidus-collab <guyfreaky894@gmail.com>
2023-10-07 22:11:12 +0300

    import components

--- Client/src/Pages/Post/PostResponsesBuilder.tsx
+++ Client/src/Pages/Post/PostResponsesBuilder.tsx
@@ -1,5 +1,9 @@
 import React, { FC, useState, useEffect } from "react";
 
+import { Box, text, Flex } from "@chakra-ui/react";
+import RichEditor from "../../utils/editior/RichTextEditior";
+import PostResponses from "./PostResponses";
+
 interface PostResponsesBuilder {}
 
 const PostResponsesBuilder = () => {
