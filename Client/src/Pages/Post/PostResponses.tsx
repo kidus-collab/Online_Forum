fe0f96fcd99fb47dcce8e6aeadd0822c90e0215d
kidus-collab <guyfreaky894@gmail.com>
2023-10-08 16:08:15 +0300

    child component of postresponse component

--- Client/src/Pages/Post/PostResponses.tsx
+++ Client/src/Pages/Post/PostResponses.tsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { FC, useState, useEffect } from "react";
+import RichEditor from "../../utils/editior/RichTextEditior";
+import UserNameandTime from "./userNameandTime";
+import ThreadPoints from "./likes&view/ThreadPoints";
+import { Box, Text, Flex } from "@chakra-ui/react";
 
-const PostResponses = () => {
-  return <div>PostResponses</div>;
+import { Font, color } from "../../utils/constants";
+
+interface PostResponsesProps {
+  userName?: string;
+  points?: number;
+  createdOn?: Date;
+  body?: string;
+}
+
+const PostResponses: FC<PostResponsesProps> = ({
+  userName,
+  points,
+  body,
+  createdOn,
+}) => {
+  return (
+    <Box>
+      <Box>
+        <UserNameandTime UserName={userName} lastModified={createdOn} />
+        <Box as="span">
+          <ThreadPoints points={points || 0} />
+        </Box>
+      </Box>
+      <RichEditor existingBody={body} />
+    </Box>
+  );
 };
 
 export default PostResponses;
