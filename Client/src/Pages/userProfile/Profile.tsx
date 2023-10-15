807c973bfd7031e267eb776dce0d83bafebe2b9f
kidus-collab <guyfreaky894@gmail.com>
2023-10-15 19:59:05 +0300

    component building

--- Client/src/Pages/userProfile/Profile.tsx
+++ Client/src/Pages/userProfile/Profile.tsx
@@ -2,14 +2,18 @@ import React, { useState, useEffect, useReducer } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
-import { Text, Flex, Box, Vstack, Button } from "@chakra-ui/react";
+import { Text, Flex, Box, VStack, Button } from "@chakra-ui/react";
 import { Font, color } from "../../utils/constants";
 //interface ProfileProps {}
 
 const Profile = () => {
   useEffect(() => {});
 
-  return <div>Profile</div>;
+  return (
+    <VStack>
+      <Box>Profile</Box>
+    </VStack>
+  );
 };
 
 export default Profile;
