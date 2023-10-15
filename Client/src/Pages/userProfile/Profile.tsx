5fe77bdf2607f527bb3c21d2806b3d0dfc8fb1b0
kidus-collab <guyfreaky894@gmail.com>
2023-10-15 19:57:00 +0300

    necessary imports

--- Client/src/Pages/userProfile/Profile.tsx
+++ Client/src/Pages/userProfile/Profile.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useState, useEffect, useReducer } from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+
+import { Text, Flex, Box, Vstack, Button } from "@chakra-ui/react";
+import { Font, color } from "../../utils/constants";
+//interface ProfileProps {}
 
 const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
-}
+  return <div>Profile</div>;
+};
 
-export default Profile
+export default Profile;
