13623cb617c73f5f69619f231279969c4c5508b6
kidus-collab <guyfreaky894@gmail.com>
2023-10-15 20:01:47 +0300

    Header added

--- Client/src/Pages/userProfile/Profile.tsx
+++ Client/src/Pages/userProfile/Profile.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useReducer } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
-import { Text, Flex, Box, VStack, Button } from "@chakra-ui/react";
+import { Text, Flex, Box, Avatar, VStack, Button } from "@chakra-ui/react";
 import { Font, color } from "../../utils/constants";
 //interface ProfileProps {}
 
@@ -10,8 +10,24 @@ const Profile = () => {
   useEffect(() => {});
 
   return (
-    <VStack>
-      <Box>Profile</Box>
+    <VStack alignItems="center" justifyContent="center">
+      <Box alignItems="left" justifyContent="left">
+        <Avatar />
+        <Button
+          pos="absolute"
+          right={0}
+          bottom={0}
+          fontSize="13px"
+          fontWeight="extrabold"
+          color="white"
+          rounded="100%"
+          w={3}
+          h="30px"
+          bgColor={color[3]}
+        >
+          +
+        </Button>
+      </Box>
     </VStack>
   );
 };
